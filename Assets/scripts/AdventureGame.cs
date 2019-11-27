68ed44be026608abdff48667dca9da73ad8684f5
John <johnehresmann3d@gmail.com>
2019-11-27 08:16:57 -0800

    added state handling

---
+++ Assets/scripts/AdventureGame.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] Text textComponent;
+    [SerializeField] State starting_state;
+
+    State state;
+    void Start()
+    {        
+        state = starting_state;
+        textComponent.text = state.GetStoryText();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/scripts/State.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+    [TextArea(10,14)] [SerializeField] string story_text;
+
+    public string GetStoryText() {
+        
+        return story_text;
+    }
+    
+}
