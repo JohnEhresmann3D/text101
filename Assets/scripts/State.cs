19ced87166c1967b9cd170eb5a25345523961bc7
John <johnehresmann3d@gmail.com>
2019-11-27 16:35:55 -0800

    Added more States

--- Assets/scripts/State.cs
+++ Assets/scripts/State.cs
@@ -7,9 +7,16 @@ public class State : ScriptableObject
 {
     [TextArea(10,14)] [SerializeField] string story_text;
 
+    [SerializeField] State[] next_state;
+
     public string GetStoryText() {
         
         return story_text;
+    }
+
+    public State[] GetNextStates() {
+
+        return next_state;
     }
     
 }
